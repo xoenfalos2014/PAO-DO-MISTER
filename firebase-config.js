17c121c3a2fe18c1281042445f9c88e728f3483e
xoenfalos2014 <XOENFALOS2014@GMAIL.COM>
2025-05-12 20:07:51 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAn9fZc97X1lyp1xtHZDLTflrplUOWK21w",
+  authDomain: "misterbraseiro-produto.firebaseapp.com",
+  databaseURL: "https://misterbraseiro-produto-default-rtdb.firebaseio.com",
+  projectId: "misterbraseiro-produto",
+  storageBucket: "misterbraseiro-produto.firebasestorage.app",
+  messagingSenderId: "936286687179",
+  appId: "1:936286687179:web:117ae2273e2025dfd9704e"
+};
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
