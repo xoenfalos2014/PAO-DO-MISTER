adcefcd7b549e37a4e84ba7a54aee0d97243c673
xoenfalos2014 <XOENFALOS2014@GMAIL.COM>
2025-05-12 17:50:28 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
